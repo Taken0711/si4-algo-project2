2bb8d54d11bed3cd9b89e2453d8ee48a19936568
piernov <piernov@piernov.org>
2018-01-05 21:06:31 +0100

    Fix fulfillment computation in analytics.py

--- analytics.py
+++ analytics.py
@@ -8,9 +8,9 @@ def main():
     for inFile, all_res in ex.items():
         with open("analytics_" + inFile + ".csv", 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=["Algorithme", "Nombre de camions utilisés", "Nombre d'accès aux camions", "Nombre de remplissage des camions",
-                    "Moyenne de remplissage d'un camion", "Camions remplis à 1-10%", "Camions remplis à 11-20%", "Camions remplis à 21-30%", "Camions remplis à 31-40%",
-                    "Camions remplis à 41-50%", "Camions remplis à 51-60%", "Camions remplis à 61-70%", "Camions remplis à 71-80%", "Camions remplis à 81-90%",
-                    "Camions remplis à 91-100%"])
+                    "Moyenne de remplissage d'un camion", "% de camions remplis à 1-10%", "% de camions remplis à 11-20%", "% de camions remplis à 21-30%", "% de camions remplis à 31-40%",
+                    "% de camions remplis à 41-50%", "% de camions remplis à 51-60%", "% de camions remplis à 61-70%", "% de camions remplis à 71-80%", "% de camions remplis à 81-90%",
+                    "% de camions remplis à 91-100%"])
             writer.writeheader()
             for res in all_res:
                 row = {}
@@ -20,10 +20,10 @@ def main():
                 row["Nombre d'accès aux camions"] = bins.bin_access
                 row["Nombre de remplissage des camions"] = bins.bin_change
                 row["Moyenne de remplissage d'un camion"] = round(100 * sum(bins) / (len(bins)*bins.bin_size))
-                for i in range(0, 90, 10):
-                    min = i + 1
-                    max = i + 10
-                    row["Camions remplis à {0}-{1}%".format(min, max)] = len(list(filter(lambda bin: min <= bin/bins.bin_size <= max, bins)))/len(bins)
+                for i in range(0, 100, 10):
+                    m_min = i + 1
+                    m_max = i + 10
+                    row["% de camions remplis à {0}-{1}%".format(m_min, m_max)] = round(100 * len(list(filter(lambda bin: m_min <= 100 * bin/bins.bin_size <= m_max, bins)))/len(bins), 2)
                 writer.writerow(row)
 
 
