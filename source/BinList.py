cbaf77217e45c811ec0b236c4b91cf7ff6586267
piernov <piernov@piernov.org>
2018-01-05 20:16:52 +0100

    Also count access to BinList with iterators (I guess this was the intended purpose)

--- source/BinList.py
+++ source/BinList.py
@@ -1,4 +1,3 @@
-
 class BinList(list):
 
     def __init__(self, size):
@@ -10,6 +9,17 @@ class BinList(list):
         self.bin_access += 1
         return super().__getitem__(key)
 
+    def __iter__(self):
+        self.m_iter = super().__iter__()
+        return self
+
+    def __next__(self):
+        try:
+            m_next = self.m_iter.__next__()
+            self.bin_access += 1
+            return m_next
+        except StopIteration: # 'cause we don't wanna count past the end
+            raise StopIteration
 
     def fit(self, index, object):
         return self.__getitem__(index) + object <= self.bin_size
